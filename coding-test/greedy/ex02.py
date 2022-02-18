b476f013ffa7a1783ad98795e2a8c17f08fe138f
junghyunpark <junghyunpark@Kate-MacBookPro.local>
2022-02-18 22:50:43 +0900

    [UPDATE] greedy

--- coding-test/greedy/ex02.py
+++ coding-test/greedy/ex02.py
@@ -40,3 +40,20 @@ while True:
     
 print(result)
 
+# 다른 풀이
+
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort(reverse=True)
+first = data[0]
+second = data[1]
+
+count = int(m / (k + 1)) * k
+count += (m % (k + 1))
+
+result = 0
+result += count * first
+result += (m - count) * second
+
+print(result)
