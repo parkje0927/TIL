0960c6bf6faa2fa89b8c9dd870e28c0c2ca46356
parkje0927 <parkje09270@gmail.com>
2022-03-02 22:11:58 +0900

    [UPDATE] python - variable

--- coding-test/greedy/_variable.py
+++ coding-test/greedy/_variable.py
@@ -28,18 +28,18 @@ print()
 
 # 변수 입력과 연산자
 
-# a = input("숫자를 입력하세요 : ")
-# print(a)
+a = input("숫자를 입력하세요 : ")
+print(a)
 
-# a, b = map(int, input("숫자를 입력하세요 : ").split())
-# print(a, b)
-# print(a + b)
-# print(a - b)
-# print(a * b)
-# print(a / b)
-# print(a // b)
-# print(a % b)
-# print(a ** b)
+a, b = map(int, input("숫자를 입력하세요 : ").split())
+print(a, b)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
 
 a = 4.3
 b = 5
